baa6510a5c7b4c41f3d4c2233e93d2d5bb981c2a
BryantDavis1986 <bryant.davis@hotmail.com>
2020-04-14 16:52:05 -0700

    replaced the Li elements with a chart, stopped any type of repeating from occuring

--- app.js
+++ app.js
@@ -2,15 +2,15 @@
 
 
 
-var allProductsArray = [];
-var rounds = 25;
+ProductImages.allProductsArray = [];
+ProductImages.rounds = 25;
 
 function ProductImages(name, imageSrc) {
     this.clickCount = 0;
     this.name = name;
     this.imageSrc = imageSrc;
     this.views = 0;
-    allProductsArray.push(this);
+    ProductImages.allProductsArray.push(this);
 }
 
 ProductImages.prototype.render = function() {
@@ -31,87 +31,96 @@ ProductImages.prototype.render = function() {
 var totalCliclsPerImage = 0;
 
 function productImageClicks (event) {
-    if(totalCliclsPerImage < rounds) {
+    if(totalCliclsPerImage < ProductImages.rounds) {
         totalCliclsPerImage++;
         
-        for(var i = 0; i < allProductsArray.length; i++) {
-            if(event.target.id === allProductsArray[i].name){
-                allProductsArray[i].clickCount++
+        for(var i = 0; i < ProductImages.allProductsArray.length; i++) {
+            if(event.target.id === ProductImages.allProductsArray[i].name){
+                ProductImages.allProductsArray[i].clickCount++
             }
         
         }
         renderProductImages();
       
     }
-    if(totalCliclsPerImage === rounds) {
+    if(totalCliclsPerImage === ProductImages.rounds) {
         var ulTarget = document.getElementById('products');
         ulTarget.innerHTML = '';
         var ctx = document.getElementById('results').getContext('2d');
+
+        var imageName = [];
+
+        for(i = 0; i < ProductImages.allProductsArray.length; i++){
+            imageName.push(ProductImages.allProductsArray[i].name);
+        }
+
+        var clickTotal = [];
+
+        for(i = 0; i < ProductImages.allProductsArray.length; i++){
+            clickTotal.push(ProductImages.allProductsArray[i].clickCount);
+        }
+
+        var viewTotal = [];
+
+        for(i = 0; i < ProductImages.allProductsArray.length; i++){
+            viewTotal.push(ProductImages.allProductsArray[i].views);
+        }
+
+
         var chart = new Chart(ctx, {
-    // The type of chart we want to create
+
     type: 'bar',
 
-    // The data for our dataset
     data: {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-        datasets: [{
-            label: 'My First dataset',
-            backgroundColor: 'rgb(255, 99, 132)',
-            borderColor: 'rgb(255, 99, 132)',
-            data: [0, 10, 5, 2, 20, 30, 45]
-        }]
-    },
+        labels: imageName,
+        datasets: [
+            {
+          label: 'Product Clicks',
+          backgroundColor: 'rgb(255, 99, 132, 0.4)',
+          borderColor: 'rgb(255, 99, 132)',
+          data: clickTotal
+        },
+        {
+          label: 'Product Views',
+          backgroundColor: 'rgb(30, 99, 132, 0.4)',
+          borderColor: 'rgb(30, 99, 132)',
+          data: viewTotal
+        }
+    ]
+      },
 
-    // Configuration options go here
     options: {}
-});
-       
-        // for (var j = 0; j < allProductsArray.length; j++) {
-        //     var ulTarget = document.getElementById('results');
-        //     var createH1 = document.createElement('h1');
-        //     var createTotalViews = document.createElement('li');
-        //     var createTotalClicks = document.createElement('li');
+}
+);
 
-        //     createTotalViews.textContent = 'Total Views: ' + allProductsArray[j].views;
-        //     createTotalClicks.textContent = 'Total Clicks: ' + allProductsArray[j].clickCount;
-        //     createH1.textContent = allProductsArray[j].name;
-        //     ulTarget.appendChild(createH1);
-        //     ulTarget.appendChild(createTotalViews);
-        //     ulTarget.appendChild(createTotalClicks);
-        // }
     }
 }
 ProductImages.random = [];
-
+ProductImages.duplicateImages = [];
 function renderProductImages() {
     // debugger;
-    console.log('starting render');
     var ulTarget = document.getElementById('products');
     ulTarget.innerHTML = '';
-    var left = Math.floor(Math.random() * allProductsArray.length);
-    
-    do {
-        var center = Math.floor(Math.random() * allProductsArray.length);
+    if(ProductImages.random){
+        ProductImages.duplicateImages = ProductImages.random;
     }
-    while(left === center); 
-    console.log(left, center);
-    ProductImages.random.push(center);
-    ProductImages.random.push(left);
-    
-    do {
-        var right = Math.floor(Math.random() * allProductsArray.length);
-    }
-    while(right === center || right === left); 
-    
-    ProductImages.random.push(right);
-    
+    ProductImages.random = [];
+    var randomImage; 
     
     for(var i = 0; i < 3; i++) {
-        // console.log(ProductImages.random[i]);
-        allProductsArray[ProductImages.random[i]].render();
-    allProductsArray[ProductImages.random[i]].views++;
+        do {
+            randomImage = Math.floor(Math.random() * ProductImages.allProductsArray.length);
+        }
+        while(ProductImages.duplicateImages.includes(randomImage) || ProductImages.random.includes(randomImage))
+        
+        ProductImages.random.push(randomImage);
+        ProductImages.allProductsArray[randomImage].render();
+        ProductImages.allProductsArray[randomImage].views++
+    }
+    console.log(ProductImages.random);
+    console.log(ProductImages.duplicateImages);
 }
-}
+
 
 var ulEl = document.getElementById('products');
 ulEl.addEventListener('click', productImageClicks);
@@ -122,20 +131,20 @@ new ProductImages('Bathroom', 'Images/bathroom.jpg');
 new ProductImages('Boots', 'Images/boots.jpg');
 new ProductImages('Breakfast', 'Images/breakfast.jpg');
 new ProductImages('Bubblegum', 'Images/bubblegum.jpg');
-// new ProductImages('Chair', 'Images/chair.jpg');
-// new ProductImages('Cthulhu', 'Images/cthulhu.jpg');
-// new ProductImages('Dog Duck', 'Images/dog-duck.jpg');
-// new ProductImages('Dragon', 'Images/dragon.jpg');
-// new ProductImages('Pen', 'Images/pen.jpg');
-// new ProductImages('Pet Sweep', 'Images/pet-sweep.jpg');
-// new ProductImages('Scissors', 'Images/scissors.jpg');
-// new ProductImages('Shark', 'Images/shark.jpg');
-// new ProductImages('Sweep', 'Images/sweep.png');
-// new ProductImages('TaunTaun', 'Images/tauntaun.jpg');
-// new ProductImages('Unicorn', 'Images/unicorn.jpg');
-// new ProductImages('USB', 'Images/usb.gif');
-// new ProductImages('Water Can', 'Images/water-can.jpg');
-// new ProductImages('Wine Glass', 'Images/wine-glass.jpg');
+new ProductImages('Chair', 'Images/chair.jpg');
+new ProductImages('Cthulhu', 'Images/cthulhu.jpg');
+new ProductImages('Dog Duck', 'Images/dog-duck.jpg');
+new ProductImages('Dragon', 'Images/dragon.jpg');
+new ProductImages('Pen', 'Images/pen.jpg');
+new ProductImages('Pet Sweep', 'Images/pet-sweep.jpg');
+new ProductImages('Scissors', 'Images/scissors.jpg');
+new ProductImages('Shark', 'Images/shark.jpg');
+new ProductImages('Sweep', 'Images/sweep.png');
+new ProductImages('TaunTaun', 'Images/tauntaun.jpg');
+new ProductImages('Unicorn', 'Images/unicorn.jpg');
+new ProductImages('USB', 'Images/usb.gif');
+new ProductImages('Water Can', 'Images/water-can.jpg');
+new ProductImages('Wine Glass', 'Images/wine-glass.jpg');
 
 
 renderProductImages();
